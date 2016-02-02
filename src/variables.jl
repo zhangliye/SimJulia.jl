eb04b05d18d31e3ac0213aa2c54ecae2a5f84082
Ben Lauwens <ben.lauwens@gmail.com>
2016-02-02 16:08:16 +0100

    Implement total derivative with respect to time

--- src/variables.jl
+++ src/variables.jl
@@ -1,13 +1,56 @@
 using Calculus
 using Polynomials
 
+import Calculus.integrate
+
 abstract AbstractVariable
 
+function integrate(ev::AbstractEvent, var::AbstractVariable)
+
+end
+
+type Step <: AbstractEvent
+  bev :: BaseEvent
+  function Step(env::AbstractEnvironment, delay::Float64, var::AbstractVariable)
+    step = new()
+    step.bev = BaseEvent(env)
+    push!(step.bev.callbacks, (ev) -> integrate(ev, var))
+    schedule(step, delay, var)
+    return step
+  end
+end
+
+function total_derivatives_with_respect_to_time(f::AbstractString, order::Int, deps...)
+  n = length(deps)
+  args = AbstractString["t", deps...]
+  derivs = Function[eval(parse("($(reduce((a::AbstractString,b::AbstractString)->"$a,$b",args)))->$f"))]
+  if order > 1
+    fun = f
+    for i = 2:order
+      ∇f = differentiate(fun, args)
+      df = AbstractString["$(∇f[1])"]
+      for j = 1:n
+        push!(args, "d$i$(deps[j])")
+      end
+      for j = 2:length(∇f)
+        push!(df, "($(∇f[j])) * $(args[j+n])")
+      end
+      fun = reduce((a::AbstractString,b::AbstractString)->"$a + $b", df)
+      push!(derivs, eval(parse("($(reduce((a::AbstractString,b::AbstractString)->"$a,$b",args)))->$fun")))
+    end
+  end
+  return derivs
+end
+
 type Continuous
+  env :: AbstractEnvironment
+  order :: Int
   vars :: Dict{AbstractString, AbstractVariable}
   rev_deps :: Dict{AbstractString, Set{AbstractVariable}}
-  function Continuous()
+  function Continuous(env::AbstractEnvironment, order::Int)
     cont = new()
+    cont.env = env
+    cont.order = order
     cont.vars = Dict{AbstractString, AbstractVariable}()
     cont.rev_deps = Dict{AbstractString, Set{AbstractVariable}}()
     return cont
@@ -21,45 +64,24 @@ type Variable <: AbstractVariable
   q :: Vector{Float64}
   derivs :: Vector{Function}
   deps :: Vector{AbstractString}
-  order :: Int
-  function Variable(cont::Continuous, name::AbstractString, x₀::Float64, f::AbstractString, deps::Vector{AbstractString}, order::Int)
+  ev :: Step
+  function Variable(cont::Continuous, name::AbstractString, delay::Float64, x₀::Float64, f::AbstractString, deps...)
     var = new()
     var.cont = cont
     cont.vars[name] = var
     var.name = name
-    var.x = Vector{Float64}()
-    push!(var.x, x₀)
-    var.q = Vector{Float64}()
-    var.derivs = Vector{Function}()
-    n = length(deps)
-    args = ""
-    for i in 1:n
-      args *= deps[i]
-      if i != n
-        args *= ","
-      end
-    end
-    push!(var.derivs, eval(parse("($args)->$f")))
-    var.deps = deps
-    for i in 1:n
+    var.x = zeros(Float64, cont.order+1)
+    var.x[1] = x₀
+    var.q = zeros(Float64, cont.order)
+    var.derivs = total_derivatives_with_respect_to_time(f, cont.order, deps...)
+    var.deps = AbstractString[deps...]
+    for i in 1:length(deps)
       if !haskey(cont.rev_deps, deps[i])
         cont.rev_deps[deps[i]] = Set{AbstractVariable}()
       end
       push!(cont.rev_deps[deps[i]], var)
     end
-    var.order = order
-    df = ""
-    if order > 1
-      ∇f = differentiate(f, deps)
-      for i in 1:n
-        args *= ",d"*deps[i]
-        df *= "$(∇f[i])*"*"d"*deps[i]
-        if i != n
-          df *= "+"
-        end
-      end
-      push!(var.derivs, eval(parse("($args)->$df")))
-    end
+    var.ev = Step(cont.env, delay, var)
     return var
   end
 end
--- test/test_variables.jl
+++ test/test_variables.jl
@@ -1,5 +1,7 @@
 using SimJulia
 
-system = Continuous()
-x₁ = Variable(system, "x₁", 0.0, "0.02*x₂^2", AbstractString["x₂"], 2)
-x₂ = Variable(system, "x₂", 20.0, "2020.0-100.0*x₁^2-100.0*x₂^2", AbstractString["x₁", "x₂"], 2)
+env = Environment()
+system = Continuous(env, 2)
+x₁ = Variable(system, "x₁", 0.0, 0.0, "0.02*x₂^2", "x₂")
+x₂ = Variable(system, "x₂", 0.0, 20.0, "2020.0-100.0*x₁^2-100.0*x₂^2", "x₁", "x₂")
+x₂.derivs[2](1.0, 2.0, 3.0, 1.0, 2.0)
