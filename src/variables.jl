19eec9acc8aad1732ebe919e5b79a2bbb1845174
Ben Lauwens <ben.lauwens@gmail.com>
2016-04-01 17:40:47 +0200

    update v0.4

--- src/variables.jl
+++ src/variables.jl
@@ -1,96 +1,65 @@
 using Calculus
 using Polynomials
 
-import Calculus.integrate
+abstract AbstractQuantizer
 
-abstract AbstractVariable
+type Variable
+  name :: UTF8String
+  ev :: Step
+  tx :: Float64
+  tq :: Float64
+  derivatives :: Vector{Function}
+  function Variable(name::AbstractString)
+    var = new()
 
-function integrate(env::AbstractEnvironment, ev::AbstractEvent, var::AbstractVariable)
-  t = now(env)
-  Δ = t - var.tx
-  for i = 1:var.cont.order
-    for j = i+1:var.cont.order
-      var.x[i] += var.x[j]*Δ^(j-i)/factorial(j-i)
-    end
+    return var
   end
+end
+
+type Quantizer <: AbstractQuantizer
+  order :: Int
+  function Quantizer(order::Int)
+    new(order)
+  end
+end
+
+type Continuous
+  quant :: AbstractQuantizer
+  vars :: Dict{UTF8String, Variable}
+  q :: Matrix{Float64}
+  function Continuous(quant::AbstractQuantizer, vars::AbstractString...)
+    cont = new()
+    cont.quant = quant
+  end
+end
+
+function initialize(env::AbstractEnvironment, ev::Start, cont::Continuous)
+  
+end
+
+type Start <: AbstractEvent
+  bev :: BaseEvent
+  function Step(env::AbstractEnvironment, cont::Continuous, delay::Float64=0.0)
+    step = new()
+    step.bev = BaseEvent(env)
+    push!(step.bev.callbacks, (ev) -> integrate(env, ev, cont, var))
+    schedule(step, delay)
+    return step
+  end
+end
+
+function integrate(env::AbstractEnvironment, ev::Step, cont::Continuous, var::Variable)
+  t = now(env)
 
 end
 
 type Step <: AbstractEvent
   bev :: BaseEvent
-  function Step(env::AbstractEnvironment, var::AbstractVariable, delay::Float64=0.0)
+  function Step(env::AbstractEnvironment, cont::Continuous, var::Variable, delay::Float64=0.0)
     step = new()
     step.bev = BaseEvent(env)
-    push!(step.bev.callbacks, (ev) -> integrate(env, ev, var))
-    schedule(step, delay, var)
+    push!(step.bev.callbacks, (ev) -> integrate(env, ev, cont, var))
+    schedule(step, delay)
     return step
-  end
-end
-
-function total_derivatives_with_respect_to_time(f::AbstractString, order::Int, deps...)
-  n = length(deps)
-  args = AbstractString["t", deps...]
-  derivs = Function[eval(parse("($(reduce((a::AbstractString,b::AbstractString)->"$a,$b",args)))->$f"))]
-  if order > 1
-    fun = f
-    for i = 2:order
-      ∇f = differentiate(fun, args)
-      df = AbstractString["$(∇f[1])"]
-      for j = 1:n
-        push!(args, "d$i$(deps[j])")
-      end
-      for j = 2:length(∇f)
-        push!(df, "($(∇f[j])) * $(args[j+n])")
-      end
-      fun = reduce((a::AbstractString,b::AbstractString)->"$a + $b", df)
-      push!(derivs, eval(parse("($(reduce((a::AbstractString,b::AbstractString)->"$a,$b",args)))->$fun")))
-    end
-  end
-  return derivs
-end
-
-type Continuous
-  env :: AbstractEnvironment
-  order :: Int
-  vars :: Dict{AbstractString, AbstractVariable}
-  rev_deps :: Dict{AbstractString, Set{AbstractVariable}}
-  function Continuous(env::AbstractEnvironment, order::Int)
-    cont = new()
-    cont.env = env
-    cont.order = order
-    cont.vars = Dict{AbstractString, AbstractVariable}()
-    cont.rev_deps = Dict{AbstractString, Set{AbstractVariable}}()
-    return cont
-  end
-end
-
-type Variable <: AbstractVariable
-  cont :: Continuous
-  name :: AbstractString
-  x :: Vector{Float64}
-  q :: Vector{Float64}
-  tx :: Float64
-  tq :: Float64
-  derivs :: Vector{Function}
-  deps :: Vector{AbstractString}
-  ev :: Step
-  function Variable(cont::Continuous, name::AbstractString, x₀::Float64, f::AbstractString, deps...)
-    var = new()
-    var.cont = cont
-    cont.vars[name] = var
-    var.name = name
-    var.x = zeros(Float64, cont.order+1)
-    var.x[1] = x₀
-    var.q = zeros(Float64, cont.order)
-    var.derivs = total_derivatives_with_respect_to_time(f, cont.order, deps...)
-    var.deps = AbstractString[deps...]
-    for i in 1:length(deps)
-      if !haskey(cont.rev_deps, deps[i])
-        cont.rev_deps[deps[i]] = Set{AbstractVariable}()
-      end
-      push!(cont.rev_deps[deps[i]], var)
-    end
-    var.ev = Step(cont.env, var)
-    return var
   end
 end
--- test/test_variables.jl
+++ test/test_variables.jl
@@ -1,7 +1,3 @@
 using SimJulia
 
 env = Environment()
-system = Continuous(env, 2)
-x₁ = Variable(system, "x₁", 0.0, "0.02*x₂^2", "x₂")
-x₂ = Variable(system, "x₂", 0.0, "2020.0-100.0*x₁^2-100.0*x₂^2", "x₁", "x₂")
-x₂.derivs[2](1.0, 2.0, 3.0, 1.0, 2.0)
